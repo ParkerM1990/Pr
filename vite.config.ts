c0ac6fc4fec7476bdb01e74d29d5466c6fde4d20
ParkerM1990 <madrzak1990@gmail.com>
2026-09-06 01:02:26 +0200

    Change base path in vite.config.ts
    Update base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // DODAJ TĘ LINIKĘ (zmień 'nazwa-twojego-repo' na nazwę z adresu URL):
-  base: '/parkerm1990.github.io/Pr/', 
+  base: '/Pr/', 
 })
